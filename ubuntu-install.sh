fc14909dd0e6ac3c4d0a5a0eb1d20881b3776dbc
Mayur Patil <ram.nath241089@gmail.com>
2018-05-13 23:40:30 +0530

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -17,14 +17,11 @@ sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
 #Update The Cache
 clear
 echo "Updating Cache..."
-sudo apt update && sudo	apt list --upgradable || OwnError "Updating Cache Failed :("  #for bionic bever
+sudo apt update && sudo	apt list --upgradable || OwnError "Updating Cache Failed :("  #for bioic bever
 
 # Install Apt-Add-Repository Python Tool
 sudo apt install software-properties-common || OwnError "Unable To Install Python Software Properties :(" 
 
-#Swachchha PPA Abhiyan
-cd /etc/apt/sources.list.d/
-rm -f * 
 
 #Google Repository 
 clear
@@ -41,17 +38,17 @@ sudo sh -c 'wget -O - http://deb.opera.com/archive.key | apt-key add -'
 #Skype Repository 
 clear
 echo "Install Repository For Skype..."
-sudo sh -c  'echo "deb [arch=amd64] http://archive.canonical.com/ubuntu/$(lsb_release -sc) partner" >> /etc/apt/sources.list.d/canonical_partner.list' || OwnError "Unable To Add Skype Repository  :("
+sudo sh -c  'echo "deb [arch=amd64] http://archive.canonical.com/ubuntu/ $(lsb_release -sc) partner" >> /etc/apt/sources.list.d/canonical_partner.list' || OwnError "Unable To Add Skype Repository  :("
 
 #NodeJs Repository
 clear
 echo "Install Repository For NodeJs..."
-sudo add-apt-repository -y ppa:chris-lea/node.js || OwnError "Unable To NodeJs Repository  :("
+#sudo add-apt-repository -y ppa:chris-lea/node.js || OwnError "Unable To NodeJs Repository  :("
 
 #Shutter Repository
 clear
 echo "Install Repository For Shutter..."
-sudo add-apt-repository -y ppa:shutter/ppa || OwnError "Unable To Add Shutter Repository  :("
+# sudo add-apt-repository -y ppa:shutter/ppa || OwnError "Unable To Add Shutter Repository  :("
 
 #Update The Cache
 clear
@@ -62,13 +59,13 @@ sudo apt update && sudo apt list --upgradable || OwnError "Updating Cache Failed
 #Install Common Softwares
 clear
 echo "Installing Git Vim Filezilla Google-Chrome Skype Oracle-Jdk Opera"
-sudo apt -y install git-core openssh-server shutter pv vim vlc \
-curl filezilla google-chrome-stable skype sni-qt sni-qt \
+sudo apt -y install git openssh-server shutter pv vim vlc \
+curl filezilla google-chrome-stable skype sni-qt \
 libasound2-plugins openjdk-8-jre icedtea-8-plugin \
 openjdk-8-jdk diodon diodon-plugins ubuntu-restricted-extras \
-p7zip-full p7zip-rar opera nautilus-open-terminal nodejs libcurl4-gnutls-dev \
-libxml2 libxml2-dev libxslt1-dev ruby-dev ruby-sass  ruby libruby || OwnError "Installation Failed :("
-# rubygems
+p7zip-full p7zip-rar opera nautilus-admin nodejs libcurl4-gnutls-dev \
+libxml2 libxml2-dev libxslt1-dev ruby-dev ruby-sass ruby libruby || OwnError "Installation Failed :("
+# skype rubygems
 
 #Install SASS
 clear
@@ -104,10 +101,10 @@ sudo npm install -g bower
 clear
 echo "Downloading Netbeans..."
 read -p "Enter latest version of Netbeans:"
-wget -c http://download.netbeans.org/netbeans/8.2/final/bundles/netbeans-8.2-php-linux-x64.sh || OwnError "Network connectivity problem :("
-chmod u+x netbeans-8.2-php-linux-x64.sh
+wget -c http://download.netbeans.org/netbeans/$late_ver/final/bundles/netbeans-$late_ver-php-linux-x64.sh || OwnError "Network connectivity problem :("
+chmod u+x netbeans-$late_ver-php-linux-x64.sh
 echo "Installing Netbeans..."
-sudo bash netbeans-8.2-php-linux.sh || OwnError "Unable to install Netbeans :("
+sudo bash netbeans-$late_ver-php-linux.sh || OwnError "Unable to install Netbeans :("
 clear
 
 
