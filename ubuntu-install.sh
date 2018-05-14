c051576b88047ac9296c5e0504cd9d357f38ce29
Mayur Patil <ram.nath241089@gmail.com>
2018-05-14 07:02:15 +0530

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -49,14 +49,14 @@ echo "Install Repository For Skype..."
 sudo sh -c  'echo "deb [arch=amd64] http://archive.canonical.com/ubuntu/ $(lsb_release -sc) partner" >> /etc/apt/sources.list.d/canonical_partner.list' || OwnError "Unable To Add Skype Repository  :("
 
 #NodeJs Repository
-clear
-echo "Install Repository For NodeJs..."
+#clear
+#echo "Install Repository For NodeJs..."
 #sudo add-apt-repository -y ppa:chris-lea/node.js || OwnError "Unable To NodeJs Repository  :("
 
 #Shutter Repository
-clear
-echo "Install Repository For Shutter..."
-sudo add-apt-repository -y ppa:shutter/ppa || OwnError "Unable To Add Shutter Repository  :("
+#clear
+#echo "Install Repository For Shutter..."
+#sudo add-apt-repository -y ppa:shutter/ppa || OwnError "Unable To Add Shutter Repository  :("
 
 #Update The Cache
 clear
@@ -69,10 +69,10 @@ clear
 echo "Installing Git Vim Filezilla Google-Chrome Skype Oracle-Jdk Opera"
 sudo apt -y install git openssh-server shutter pv vim vlc \
 curl filezilla google-chrome-stable skype sni-qt \
-libasound2-plugins oracle-java8-installer \
+libasound2-plugins openjdk-8-jre icedtea-8-plugin openjdk-8-jdk \
 p7zip-full p7zip-rar opera nautilus-admin nodejs libcurl4-gnutls-dev \
 libxml2 libxml2-dev libxslt1-dev ruby-dev ruby-sass ruby libruby || OwnError "Installation Failed :("
-# skype rubygems #openjdk-8-jre icedtea-8-plugin \openjdk-8-jdk diodon diodon-plugins ubuntu-restricted-extras
+# skype rubygems openjdk-8-jre icedtea-8-plugin openjdk-8-jdk diodon diodon-plugins ubuntu-restricted-extras
 
 #Install SASS
 clear
